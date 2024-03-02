a885ae8c611679a86072dff2ed85f458ba346581
amch-med23 <skyied.med2023@gmail.com>
2024-03-02 16:13:44 +0100

    adding the email_confirmation logic

---
+++ react_frontend/src/componenets/email_confirmation.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import axios from 'axios';
+import { useNavigate, useLocation } from 'react-router-dom';
+
+import NavBar from './navbar';
+import Footer from './footer';
+
+import '../styling/index_styling.css';
+
+function EmailConfirmation(){
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const apiEndPoint = "http://wsl.localhost:5000/api/v1/"
+
+    let user_email = location.state.email;
+    let session_id = location.state.session_id;
+    let user_name = location.state.user_name;
+    let user_password = location.state.password;
+
+    console.log('email: ' + user_email + ', session_id: ' + session_id);
+
+    function handleVerification(event){
+        event.preventDefault();
+        let ver_code = document.getElementById('verification_code').value;
+        console.log('verification in process ... ver code : ' + ver_code);
+        let ver_obj_data = {}
+        let available_attr = ['email', 'session_id', 'ver_code', 'user_name', 'password']
+        let ver_data = [user_email, session_id, ver_code, user_name, user_password]
+
+        let i = 0
+        while (i < 5){
+            ver_obj_data[available_attr[i]] = ver_data[i];
+            i = i + 1;
+        }
+        // sending the ver_obj_data
+        axios.post(apiEndPoint.concat('verify_email'), ver_obj_data).then( res => {
+            console.log(res.data['verification_status']) //value = verified or not_verified
+            if (res.data['verification_status'] === 'verified'){
+                let code_paragraph_error = document.getElementById('ver_code_error');
+                code_paragraph_error.innerText = '';
+                navigate('/register_success');
+            }
+            else{
+                let code_paragraph_error = document.getElementById('ver_code_error');
+                code_paragraph_error.innerText = 'your verification code is wrong, please try again';
+
+            }
+        })
+        
+    }
+
+    return(
+        <>
+        <NavBar />
+            <div className='container'>
+                <section>
+                    <p>This is the email verification page</p> <br />
+                    <div className='form_error_class' id='ver_code_error'></div>
+                    <br />
+                    <form onSubmit={handleVerification}>
+                        <h>Enter the code you received in your email:</h><br /><br />
+                        <div className='input_box'>
+                        <input id="verification_code" className='email_verificaion' minLength={6} maxLength={6} type='text' required ></input>
+                        </div>
+                        <br /> <br />
+                        <button type='submit'>Verify</button>
+                    </form>
+                </section>
+            </div>
+        <Footer />
+        </>
+    )
+}
+
+export default EmailConfirmation
