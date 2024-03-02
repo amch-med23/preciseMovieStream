c9a370449fad897c0e57e516554afd7e877d1be3
amch-med23 <skyied.med2023@gmail.com>
2024-03-02 16:15:39 +0100

    updating the footer and the navbar for the home componenet

---
+++ react_frontend/src/componenets/home_footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styling/nav_styling.css';
+
+import '../styling/footer_styling.css';
+
+export default class HomeFooter extends React.Component {
+
+    componentDidMount(){
+
+    }
+
+    render()
+    {
+        return (
+            <div className="footer">
+            
+            </div>
+        )
+    }
+    
+
+}
---
+++ react_frontend/src/componenets/home_navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import '../styling/nav_styling.css';
+import { Link, useNavigate } from 'react-router-dom';
+
+import '../styling/nav_styling.css';
+import '../styling/index_styling.css'
+ 
+function HomeNavBar() {
+
+    const navigate = useNavigate();
+
+    function handleClick(){        
+        localStorage.removeItem('pms_user_name');
+        localStorage.removeItem('pms_login_token');
+        console.log('you are logging out..., the locall storage data wil be destroyed ...');
+        navigate('/');
+    }
+    
+    let current_user_name = localStorage.getItem('pms_user_name');
+    let current_login_token = localStorage.getItem('pms_login_token');
+
+    if( current_user_name != null && current_login_token != null){
+          return (
+                <div className='home_navbar'>
+                    <img src='src/assets/sand_tech.png' className='logo'></img>
+                    
+                    <button onClick={ handleClick} className='navbar_btn'>log out</button>
+                </div>
+            )
+    }
+    else{
+        return (
+            <div className="navbar">
+                <h></h>
+            </div>
+        )
+    }
+
+}
+
+export default HomeNavBar
