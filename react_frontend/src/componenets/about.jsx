fc33fc3afa7030c0249dc2085467879d01e7a057
amch-med23 <skyied.med2023@gmail.com>
2024-03-02 16:28:42 +0100

    updating the views elements of the front-end app

--- react_frontend/src/componenets/about.jsx
+++ react_frontend/src/componenets/about.jsx
@@ -2,14 +2,25 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
+import NavBar from "./navbar";
+import Footer from "./footer";
+
+import '../styling/index_styling.css';
 
 function About()
 {
     return(
         <>
-            <h>This is the About page.</h> 
-            <br />
-            <Link to="/"> <button>Go Back</button></Link>
+        <NavBar />
+        <div className="container">
+            <section>
+                <h>This is the About page.</h> 
+                <br />
+                <Link to="/"> <button>Go Back</button></Link>
+            </section>
+        </div>
+        <Footer />
+        
         </>
     )
 }
--- react_frontend/src/componenets/home.jsx
+++ react_frontend/src/componenets/home.jsx
@@ -1,15 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
+import HomeNavBar from "./home_navbar";
+import HomeFooter from "./home_footer";
+
+import '../styling/index_styling.css';
 
 function Home()
 {
+    const location = useLocation();
+    const navigate = useNavigate(); 
+
+    const user_name = localStorage.getItem('pms_user_name');
+    const user_email = location.state.user_email;
+    // make sure when logout to destroy the 'pms_user_name' and 'pms_login_token'.
+
+    console.log('you have loged in as: ' + user_name);
+    console.log('your loged in email is: ' + user_email);
+
+    //let name_paragrarph = document.getElementById('intr_user_name');
+    //name_paragrarph.innerHTML = '<div style="color: red;">user_name</div>';
+
+    function quationaire_result_submit() {
+        //window.alert("redirecting to result_page, with the username and email address, with an additional parameter 'quationaire:true' to show you the submit form.");
+        alert("redirecting to result_page, with the username and email address, with an additional parameter 'quationaire:true' to show you the submit form.");
+        navigate('/quationair_results'); //we need to pass the params in here.
+    }
+
+    function random_result_submit(){
+       //window.alert('redirecting to result_page, with no parameters.');
+       alert("redirecting to result_page, with no parameters.");
+       navigate('/random_results');
+    }
+
+
+    // so in here we just need to check if the 'pms_user_name' and 'pms_ogi_token' are not null befour we handle the user data.
     return(
         <>
-            <h>This is the Home page. It represants the control panel of the user (where he can interact with the services this system provides)</h> 
-            <br />
-            <Link to="/"> <button>Go Back</button></Link>
+            <HomeNavBar />
+            <div className="container">
+                <section className="home_welcom_section">
+                    <br />
+                    <h style={{color: "black"}}>Welcome back&nbsp; <b><h id="inter_user_name" style={{color: "red"}}> {user_name}</h></b> ,we are glad you are here.</h>
+                    <br/> <br />
+
+                </section>
+                <section className="home_main_selection_section">
+                    <div className="random_movie_div">
+                        <button onClick={ random_result_submit }>See random mvoies from random categories</button>
+                        <p> By clicking this you will see the result of a random movies or series list that you can watch. <h style={{color :"red"}}>remember this has no link to your interrests. The results are random</h></p>
+                    </div>
+                    <div className="quationaire_movie_div">
+                        <button onClick={ quationaire_result_submit }>Submit your interests</button>
+                        <p>By clicking this you will be submitting a form so we can know you better. and this will result in a list of movies and series recommanded for you.</p>
+                    </div>
+                </section>
+
+            </div>
+            
+            
         </>
     )
 }
