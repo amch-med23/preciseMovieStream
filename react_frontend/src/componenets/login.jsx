2eae6436aee9d7978520acbd13a0f007ee7dcdc5
amch-med23 <skyied.med2023@gmail.com>
2024-03-02 16:27:21 +0100

    updating the auth logic for the front-end app

--- react_frontend/src/componenets/login.jsx
+++ react_frontend/src/componenets/login.jsx
@@ -1,15 +1,95 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import axios from 'axios';
 
+import '../styling/index_styling.css';
+
+import NavBar from "./navbar";
+import Footer from "./footer";
 
 function Login()
 {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const apiEndPoint = "http://wsl.localhost:5000/api/v1/";
+
+    function handlelogIn(event){
+        event.preventDefault();
+        let user_email = document.getElementById('user_email').value;
+        let user_password = document.getElementById('user_password').value;
+
+        //console.log('user email: ' + user_email + ', user_password: ' + user_password);
+        let login_obj = {};
+        let available_attr = ['user_email', 'user_password'];
+        let form_values = [user_email, user_password];
+        
+        let i = 0;
+        while (i < 2){
+            login_obj[available_attr[i]] = form_values[i];
+            i = i + 1;
+        }
+        console.log('login object elems are: user_email: '+ login_obj['user_email'], 'user_password: ' + login_obj['user_password']);
+        axios.post(apiEndPoint.concat('login'), login_obj).then(res => {
+            console.log(res.data['login_creds_check']);
+            if (res.data['login_creds_check'] === "passed"){
+                //extract the user_name from the data package and passe it to /Home.
+                // removing the error text if there is any
+                let login_error_paragraph = document.getElementById('login_error');
+                login_error_paragraph.innerText = "";
+
+                let login_token = res.data['login_token'];
+                let user_name = res.data['user_name'];
+                let user_email = res.data['user_email']
+
+                console.log('passed');
+                console.log('your login token is : ' + login_token);
+                // first we set the login_token, then we verify it, then we van navigate to '/home'
+                //set login_token in the localStorage objet. (this will define the home navbar (login/logout buttons) [this token should be destroyed when logedout]).
+                localStorage.setItem('pms_login_token', login_token);
+                localStorage.setItem('pms_user_name', user_name);
+
+                let active_login_token = localStorage.getItem('pms_login_token');
+                if (active_login_token == login_token){
+                    navigate('/home', {'state':{'user_name': user_name, 'user_email': user_email}});
+                } else{
+                    alert('there was errors while setting the login_token variable');
+                }
+                //verify the login_token value then navigate to the '/home' route.
+                 
+            }
+            else if (res.data['login_creds_check'] === 'failed'){
+                let login_error_paragraph = document.getElementById('login_error');
+                login_error_paragraph.innerText = "the email or the password you supplied is wrong.";
+            }
+        });
+        // just a note, but we can use locastorage to save the login_status, when the user in loged in.
+        // let name_value = localStorage.getItem('name'), or .setItem('key', 'value'), or .removeItem('key')
+    }
     return(
         <>
-            <h>This is the Login page.</h> 
-            <br />
-            <Link to="/"> <button>Go Back</button></Link>
+        <NavBar />
+        <div className="container">
+                <section>
+                    <br />
+                    <h>This is the Login page, use your credentials to login</h> 
+                    <br /> <br />
+                    <p id="login_error" className="form_error_class"></p>
+                    <form onSubmit={handlelogIn}>           
+                        <div className="input_box">
+                            <h>Enter your email:</h><br />
+                            <input type="email" required id='user_email'></input>
+                        </div>
+                        <div className="input_box">
+                            <h>Enter your password:</h><br />
+                            <input type="password" required minLength={8} id="user_password"></input>
+                            <br></br>
+                        </div>
+                        <button type="submit">log in</button>
+                    </form>
+                </section>
+        </div>
+        <Footer />
         </>
     )
 }
--- react_frontend/src/componenets/register.jsx
+++ react_frontend/src/componenets/register.jsx
@@ -1,15 +1,112 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import '../styling/index_styling.css';
+import axios from "axios";
 
+import NavBar from './navbar.jsx';
+import Footer from './footer.jsx';
 
 function Register()
 {
+    const navigate = useNavigate();
+
+    function handleSubmit(event){
+        event.preventDefault() // this is to prevent the default form behaiviour
+        let user_name =  document.getElementById('user_name').value;
+        let email = document.getElementById('email').value;
+        let passwd =  document.getElementById('password').value;
+        let passwd_conf =  document.getElementById('password_conf').value;
+        let user_data = {}; // empty user dictionary
+        let available_attr = ['user_name', 'email', 'password', 'password_conf'];
+        let form_data = [user_name, email, passwd, passwd_conf];
+        
+        // populating the user_data dictionary
+        let i = 0;
+        while(i < 4){
+            user_data[available_attr[i]] = form_data[i];
+            i = i + 1;
+        };
+       
+        console.log(user_data); // testing the user data.
+        // sending the user data to the back end.
+        
+        const apiEndPoint = "http://wsl.localhost:5000/api/v1/";
+        axios.post(apiEndPoint.concat('register'), user_data).then(res => {
+            console.log("the back_end status of email is: " + res.data['email_check']);
+            console.log("the back_end status of password is : " + res.data['password_check']);
+            // the returned result check.
+            if (res.data['password_check'] === 'failed'){
+                console.log("password doesn't match, try again.");
+                let password_error_value = "password combination doesn't match, try again.";
+                const pass_error_paragraph = document.getElementById('form_pass_error_msg');
+                pass_error_paragraph.innerText = password_error_value;
+            } 
+            // this is to clear the password error message area
+            else if(res.data['password_check'] === 'passed')
+            {
+                const pass_error_paragraph = document.getElementById('form_pass_error_msg');
+                pass_error_paragraph.innerText = '';
+            }
+            if (res.data['email_check'] === 'failed'){
+                console.log("email was already used");
+                let email_error_value = "the email provided is already used, try a differant email, or reset you password, and use it to login.";
+                const email_error_paragraph = document.getElementById('form_email_error_msg');
+                email_error_paragraph.innerText = email_error_value;
+            }
+            // this is to clear the email error message area
+            else if(res.data['email_check'] === 'passed'){
+                const email_error_paragraph = document.getElementById('form_email_error_msg');
+                email_error_paragraph.innerText = "";
+            }
+            if (res.data['email_check'] === 'passed' && res.data['password_check'] === "passed"){
+                let user_email = res.data['user_email'];
+                let session_id = res.data['session_id'];
+                navigate('/email_confirmation', {'state':{'email': user_email, 'session_id': session_id, 'user_name': user_name, 'password':passwd}});
+            }
+            
+        });
+    }
+
     return(
         <>
-            <h>This is the Register page.</h> 
-            <br />
-            <Link to="/"> <button>Go Back</button></Link>
+            <NavBar />
+            <div className="container">
+                <section>
+                    <div className="register_form">
+                            <h >This is the Register page.</h> 
+                            <br /> <br />
+                            <div id="form_pass_error_msg" className="form_error_class"></div> 
+                            <div id="form_email_error_msg" className="form_error_class"></div>
+                            <br />
+                            <form onSubmit={handleSubmit}>
+
+                                <div className="input_box">
+                                    <h className="input_header">User name:</h> <br />
+                                    <input type="text" id="user_name" required placeholder="enter your user name"></input>
+                                </div>
+
+                                <div className="input_box">
+                                    <h className="input_header">Email address:</h> <br />
+                                    <input type="email" id="email" required placeholder="enter your email"></input>
+                                </div>
+                                
+                                <div className="input_box">
+                                    <h className="input_header">Password:</h> <br />
+                                    <input type="password" id="password" required minLength={8} placeholder="enter your password"></input>
+                                </div>
+                            
+                                <div className="input_box">
+                                    <h className="input_header">Confirm your password:</h> <br />
+                                    <input type="password" id="password_conf" required minLength={8} placeholder="reenter your password"></input>
+                                </div>
+                            
+                                <button type="submit">Submit</button>
+                            </form>
+                    </div>
+                </section>
+            </div>
+           <Footer />
         </>
     )
 }
