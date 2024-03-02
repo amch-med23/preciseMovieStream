64be1bbd212b8a27b06026da9e891c7215669fc5
amch-med23 <skyied.med2023@gmail.com>
2024-03-02 16:17:17 +0100

    adding thequationaire_page

---
+++ react_frontend/src/componenets/quationaire_page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import axios  from "axios";
+
+import '../styling/index_styling.css';
+import HomeNavBar from "./home_navbar";
+
+function QuationairePageHolder(){
+    return(
+        <>
+        <HomeNavBar />
+        <p>This is the quationaire page:</p>
+        </>
+    )
+}
+
+export default QuationairePageHolder
