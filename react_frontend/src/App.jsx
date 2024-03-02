93da654dcf839a7cb42f22d7e6287e3ed1afaf89
amch-med23 <skyied.med2023@gmail.com>
2024-03-02 16:26:29 +0100

    updating the main entry points for the front-end app

--- react_frontend/src/App.jsx
+++ react_frontend/src/App.jsx
@@ -1,11 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
-import './App.css'
+//import './App.css'
+import './styling/index_styling.css';
 import React from 'react';
 import Landing from './componenets/landing';
 import Home from './componenets/home';
 import Login from './componenets/login';
 import Register from './componenets/register';
 import About from './componenets/about';
+import GuestProceed from './componenets/guest_proceed';
+import GuestHome from './componenets/guest_home';
+import EmailConfirmation from './componenets/email_confirmation';
+import RegisterSuccess from './componenets/register_success';
+import RandomResultHolder from './componenets/random_result_page';
+import QuationairePageHolder from './componenets/quationaire_page';
 
 function App() {
 
@@ -17,6 +24,13 @@ function App() {
         <Route path='/register' element={ <Register /> } />
         <Route path='/home' element={ <Home />} />
         <Route path='/about' element={ <About /> } />
+        <Route path='/guest_proceed' element={ <GuestProceed />} />
+        <Route path='/guest_home' element={ <GuestHome />} />
+        <Route path='/email_confirmation' element={ <EmailConfirmation />} />
+        <Route path='/register_success' element={ <RegisterSuccess /> } />
+        <Route path='/random_results' element={<RandomResultHolder />} />
+        <Route path='/quationair_results' element={ <QuationairePageHolder />} />
+
       </Routes>
     </>
   )
