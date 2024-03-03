49034dda52ffce79980ad66ff5767689f1740d14
amch-med23 <skyied.med2023@gmail.com>
2024-03-03 17:28:44 +0100

    updating the styling of both home componenet and vabar component

--- react_frontend/src/componenets/landing.jsx
+++ react_frontend/src/componenets/landing.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
-import SystemStatus from "../scripts/system_status.jsx";
+
 // importing styling in here
 import '../styling/index_styling.css';
 
@@ -20,18 +20,18 @@ function Landing()
                     <section className="landing_section">
                         <div className="inner_div">
                         {/*<img src="src/assets/bat_man.jpg" className="main_img"></img>*/}
-                        <br />
-                        <h>Welcom to the landing page of <b>Precise Movie Stream.</b></h> 
-                        <br /> <br />
-                        <h> This service helps you chose the best movie or serie for you to watch based on your answers to a specific quationaire.</h>
-                        <br /> <br />
-                        <h> There are many more functionalities planned for this system, but this is an MVP(Minimal Viable Project) for now.</h>
-                        <br/> <br/>
-                        <h> The user can create an account or proceed as a guest so he can use the system functionalities.</h>
-                        <br />
-                        <br />
-                        <Link to="/guest_proceed"><button>Proceed as a guest</button></Link>
-                        
+                            <br />
+                            <h>Welcom to <b>Precise Movie Stream.</b></h> 
+                            <br /> <br />
+                            <h> This service helps you chose the best movie or serie for you to watch based on your answers to a specific questionnaire.</h>
+                            <br /> <br />
+                            <h> There are many more functionalities planned for this system, but this is an MVP(Minimal Viable Project) for now.</h>
+                            <br/> <br/>
+                            <h> The user can create an account or proceed as a guest so he can use the system functionalities.</h>
+                            <br />
+                            <br />
+                            <Link to="/guest_proceed"><button>Proceed as a guest</button></Link>
+                            
                         </div>
                         
                     </section>
