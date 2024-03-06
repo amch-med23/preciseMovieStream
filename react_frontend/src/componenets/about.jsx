b0f12df0e84b0a1ed728b21e4ba0ea03e65db112
amch-med23 <skyied.med2023@gmail.com>
2024-03-07 00:46:31 +0100

    adding the first test version of about page, this is surelly gonna be modified.

--- react_frontend/src/componenets/about.jsx
+++ react_frontend/src/componenets/about.jsx
@@ -14,9 +14,20 @@ function About()
         <NavBar />
         <div className="container">
             <section>
-                <h>This is the About page.</h> 
-                <br />
-                <Link to="/"> <button>Go Back</button></Link>
+                <div>
+                    <h>This app is a Movie Recomendation System.</h>
+                </div>
+                <div>
+                    <h>We are using OMDB database to get the data. </h>
+                </div>
+                <div> Made by: <h style={{color: "red"}}>Mohamed Amouch</h> ,a software engneering student @ Holbertoon school and ALX africa.
+                    <h> My github: <a target="blank" href="https://github.com/amch-med23">GITHUB</a></h>
+                    <h> Connect with me on linked in: <Link to="https://linkedin.com/in/mohamed-amouch" target="blank">LINKEDIN</Link></h>
+                </div>
+
+                <div>
+                    <h>This app repository: <Link to="https://github.com/amch-med23/preciseMovieStream" target="blank">APP REPO ON GITHUB</Link></h>
+                </div>
             </section>
         </div>
         <Footer />
