8ac07c35dd22ec3496ac69a7c8f7298a9c1c1fa7
amch-med23 <skyied.med2023@gmail.com>
2024-03-07 22:39:34 +0100

    modifying the app.py file

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,13 +2,23 @@
 """ API entry point """
 
 from flask_cors import CORS
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, abort, request
 from api.v1.views import app_views
+from flask_mail import Mail
 
-
-app = Flask(__name__)
-app.url_map.strict_slashes = False
+app = Flask(__name__)    
+app.url_map.strict_slashes = False 
 cors = CORS(app, resources= {r'/*': {'origins': '*'}})
+
+
+# defining the app configurations for the emailing functionality
+app.config['MAIL_SERVER'] = 'live.smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USERNAME'] = 'api'
+app.config['MAIL_PASSWORD'] = '5c6e18c89607cad63b8d28123ef8bbde'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
 
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
