50f6d3abb2554c6dd5bc8bbdc5fb05b8b3fe1c1e
amch-med23 <skyied.med2023@gmail.com>
2024-03-13 12:42:33 +0000

    updating the App.jsx to append the new pages

--- react_frontend/src/App.jsx
+++ react_frontend/src/App.jsx
@@ -2,18 +2,22 @@ import { Routes, Route } from 'react-router-dom';
 //import './App.css'
 import './styling/index_styling.css';
 import React from 'react';
-import Landing from './componenets/landing';
-import Home from './componenets/home';
-import Login from './componenets/login';
-import Register from './componenets/register';
-import About from './componenets/about';
-import GuestProceed from './componenets/guest_proceed';
-import GuestRandomMoviesPage from './componenets/guest_random_movies.jsx';
-import EmailConfirmation from './componenets/email_confirmation';
-import RegisterSuccess from './componenets/register_success';
-import RandomResultHolder from './componenets/random_result_page';
-import QuationairePageHolder from './componenets/quationaire_page';
-import MediaInfos from './componenets/media_infos';
+import Landing from './components/landing.jsx';
+import Home from './components/home.jsx';
+import Login from './components/login.jsx';
+import Register from './components/register.jsx';
+import About from './components/about.jsx';
+import GuestProceed from './components/guest_proceed.jsx';
+import GuestRandomMoviesPage from './components/guest_random_movies.jsx';
+import EmailConfirmation from './components/email_confirmation.jsx';
+import RegisterSuccess from './components/register_success.jsx';
+import RandomResultHolder from './components/random_result_page.jsx';
+import QuationairePageHolder from './components/quationaire_page.jsx';
+import MediaInfos from './components/media_infos.jsx';
+import GenericNotFound from './components/not_found.jsx';
+import ResetPassword from './components/password_reset.jsx';
+import PasswordResetProceed from './components/password_reset_process.jsx';
+import PasswordResetSuccess from './components/password_reset_success.jsx';
 
 function App() {
 
@@ -31,7 +35,11 @@ function App() {
         <Route path='/register_success' element={ <RegisterSuccess /> } />
         <Route path='/random_results' element={<RandomResultHolder />} />
         <Route path='/quationair_results' element={ <QuationairePageHolder />} />
-        <Route path='media_infos' element={ <MediaInfos />} />
+        <Route path='/media_infos' element={ <MediaInfos />} />
+        <Route path='/password_reset' element={<ResetPassword />} />
+        <Route path='/password_reset_process' element={ <PasswordResetProceed /> }/>
+        <Route path='/password_reset_success' element={ <PasswordResetSuccess />} />
+        <Route path='*' element={<GenericNotFound />} />
       </Routes>
     </>
   )
